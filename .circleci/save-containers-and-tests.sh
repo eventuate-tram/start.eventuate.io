9bf2f9b7d2d5662afa4aefc0ddd927b510815ed9
Chris Richardson <chris@chrisrichardson.net>
2020-03-20 17:19:57 -0700

    Fixed saving of containers

--- .circleci/save-containers-and-tests.sh
+++ .circleci/save-containers-and-tests.sh
@@ -5,10 +5,10 @@
 DIR=/root/container-logs
 
 sudo mkdir -p ${DIR}
-sudo bash -c "docker ps -a > ${DIR}/containers.txt"
+docker ps -a | sudo bash -c "cat > ${DIR}/containers.txt"
 
 for container in $(docker ps -a --format '{{.Names}}' ) ; do
-  sudo bash -c "docker logs \"$container\" > \"${DIR}/${container}.log\""
+  docker logs "$container" | sudo bash -c "cat > \"${DIR}/${container}.log\""
 done
 
 sudo bash -c "find $DIR -type f -exec chown circleci {} \;"
