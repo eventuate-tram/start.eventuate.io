289b02cfc4f4a5342ef4fa67827fc338ba993742
Chris Richardson <chris@chrisrichardson.net>
2020-03-20 17:26:45 -0700

    Publish docker image

--- .circleci/publish-docker-images.sh
+++ .circleci/publish-docker-images.sh
@@ -3,3 +3,5 @@
 docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
 
 docker tag eventuateio/start-eventuate-io:latest eventuateio/start-eventuate-io:BUILD-${CIRCLE_BUILD_NUM?}
+
+docker push eventuateio/start-eventuate-io:BUILD-${CIRCLE_BUILD_NUM?}
